90e9f03aedf15ca4db9982b6efa009d5c5a76413
Ryan Brockley <rybrockers@yahoo.com>
2019-01-27 14:56:46 +0000

    Changes to signup logic

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
 
 
 });
 
 
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model('User', userSchema);
 module.exports = User;
